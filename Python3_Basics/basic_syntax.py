9933120b8a9752225fbf97f8fb29f87c42001e62
kmtgy <kristen_thompson@goodyear.com>
2021-11-19 09:46:22 -0500

    Basic_syntax
    File with basic syntax and knowledge containing control flow, list, loops.

---
+++ Python3_Basics/basic_syntax.py
@@ -0,0 +1,51 @@
+# This program prints Hello, world!
+
+print('Hello, world!')
+
+#Python syntax basics
+
+#Variables
+
+variable_name = "String"
+variable_name_float = 1.4464
+variable_name_integer = 4
+
+#List - Lists are used to store multiple items in a single variable
+#Tuple - Cannot be changed once defined
+
+list_num = [1,2,3,4]
+tup_num = (1,2,3,4)
+
+print(list_num)
+print(tup_num)
+
+#Dictionary
+
+dict = {"name": 19,
+        "name1": 20,
+         "name2": 21}
+
+print(dict.items())
+print(dict.keys())
+print(dict.values())
+
+#Array
+
+x = [1,2,3,4,5,6]
+for number in x:
+    print(number)
+
+#Length of the array
+print(len(x))
+
+#Boolean expressions
+
+"""
+==: Equals
+!=: Not equals
+>: Greater than
+>=: Greater than or equal to
+<: Less than
+<=: Less than or equal to 
+"""
+
