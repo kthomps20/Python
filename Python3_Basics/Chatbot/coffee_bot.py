65621fc9d3f6ba47ed8845488cfea7739e566693
kmtgy <kristen_thompson@goodyear.com>
2022-01-03 14:45:11 -0500

    update

---
+++ Python3_Basics/Chatbot/coffee_bot.py
@@ -0,0 +1,22 @@
+# Define your functions
+def coffee_bot():
+  print("Welcome to the cafe!")
+coffee_bot()
+
+
+def get_size():
+  res = input('What size drink can I get for you?        \n[a] Small \n[b] Medium \n[c] Large \n>')
+  if res == 'a':
+    return 'small'
+  elif res == 'b':
+    return 'medium'
+  elif res == 'c':
+    return 'large'
+  else:
+    print_message()
+    return get_size()
+
+def print_message():
+  print('I\'m sorry, I did not understand your selection. Please enter the corresponding letter for your response.')
+
+get_size()
